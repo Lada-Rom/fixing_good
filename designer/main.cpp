3fed059fb11173066837b8dba6a2b1bd823086e7
Lada-Rom <doodleztoon@gmail.com>
2023-04-17 00:44:23 +0300

    UPD: code restructured (classes Window & Surface)

--- designer/main.cpp
+++ designer/main.cpp
@@ -1,39 +1,98 @@
-//Using SDL and standard IO
+#include <string>
+#include <iostream>
+
 #include <SDL.h>
 #include <stdio.h>
 
-//Screen dimension constants
+
+class Surface {
+public:
+	Surface() {}
+	Surface(SDL_Surface* new_surface) {
+		surface_ = new_surface;
+	}
+	~Surface() {
+		SDL_FreeSurface(surface_);
+		surface_ = nullptr;
+	}
+
+	void FillRect(const SDL_Rect* rect,
+		const SDL_PixelFormat* format,
+		uint8_t r, uint8_t g, uint8_t b) {
+		SDL_FillRect(surface_, rect, SDL_MapRGB(format, r, g, b));
+	}
+
+	SDL_Surface* ptr() {
+		return surface_;
+	}
+
+	const SDL_PixelFormat* Format() {
+		return surface_->format;
+	}
+
+private:
+	SDL_Surface* surface_{ nullptr };
+};
+
+
+class Window {
+public:
+	Window() {}
+	~Window() {
+		SDL_DestroyWindow(window_);
+		window_ = nullptr;
+	}
+
+	void Create(
+		const std::string& name,
+		const int32_t& x, const int32_t& y,
+		const int32_t& width, const int32_t& height,
+		const uint32_t& flags) {
+		window_ = SDL_CreateWindow(name.c_str(),
+			x, y, width, height, flags);
+	}
+
+	bool IsNullptr() {
+		return window_ == nullptr;
+	}
+
+	Surface GetSurface() {
+		return { SDL_GetWindowSurface(window_) };
+	}
+
+	void UpdateSurface() {
+		SDL_UpdateWindowSurface(window_);
+	}
+
+	SDL_Window* ptr() {
+		return window_;
+	}
+
+private:
+	SDL_Window* window_{ nullptr };
+};
+
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[]) {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	Window window;
+	Surface screenSurface;
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
 	else {
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL) {
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
+		window.Create("SDL Tutorial",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window.IsNullptr())
+			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
 		else {
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+			screenSurface = window.GetSurface();
+			screenSurface.FillRect(nullptr, screenSurface.Format(), 225, 255, 128);
+			window.UpdateSurface();
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 225, 255, 128));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Hack to get window to stay up
 			SDL_Event e;
 			bool quit = false;
 			while (quit == false) {
@@ -44,10 +103,6 @@ int main(int argc, char* args[]) {
 		}
 	}
 
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
 	SDL_Quit();
 
 	return 0;
