ec8deaf1eaabb2cc8d9be20552a899bc89e6e061
Lada-Rom <doodleztoon@gmail.com>
2023-04-27 20:42:21 +0300

    UPD: SDL2pp test launch (SDL2pp.dll hand-copied)

--- game/game.cpp
+++ game/game.cpp
@@ -1,39 +1,37 @@
 #include <string>
 #include <iostream>
 
-#include <SDL.h>
-#include <SDL2pp/Surface.hh>
+#include <SDL2pp/SDL2pp.hh>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include <SDL.h>
+
+namespace pp = SDL2pp;
 
 int main(int argc, char* args[]) {
-//try {
-//  if (SDL_Init(SDL_INIT_VIDEO) < 0)
-//    throw std::runtime_error("SDL initialization failed");
-//
-//  Window window("SDL Tutorial",
-//    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-//    SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-//  Surface screenSurface = window.GetSurface();
-//  screenSurface.FillRect(nullptr, screenSurface.Format(), 225, 255, 128);
-//  window.UpdateSurface();
-//
-//  SDL_Event e;
-//  bool quit = false;
-//  while (quit == false) {
-//    while (SDL_PollEvent(&e)) {
-//      if (e.type == SDL_QUIT) quit = true;
-//    }
-//  }
-//
-//  SDL_Quit();
-//
-//  return 0;
-//}
-//catch (const std::exception& ex) {
-//  std::cout << ex.what() << std::endl;
-//}
-  SDL2pp::Rect rect(0, 0, 12, 24);
+try {
+  pp::SDL sdl(SDL_INIT_VIDEO);
+
+  pp::Window window("SDL2pp demo",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+    640, 480,
+    SDL_WINDOW_RESIZABLE);
+
+  pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+  pp::Texture sprites(renderer, "../../data/player.png");
+
+  renderer.Clear();
+  renderer.Copy(sprites);
+  renderer.Present();
+
+  SDL_Delay(5000);
+
   return 0;
+}
+catch (pp::Exception& e) {
+  std::cerr << "Error in: " << e.GetSDLFunction() << std::endl;
+  std::cerr << "  Reason: " << e.GetSDLError() << std::endl;
+}
+catch (std::exception& e) {
+  std::cerr << e.what() << std::endl;
+}
 }
