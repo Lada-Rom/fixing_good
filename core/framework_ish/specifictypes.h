da29479b00b1949d5cbe884fb40382fe6624e4a5
Lada-Rom <doodleztoon@gmail.com>
2023-05-03 23:26:39 +0300

    FIX: extra spaces removed

--- core/framework_ish/specifictypes.h
+++ core/framework_ish/specifictypes.h
@@ -12,26 +12,26 @@ namespace user_utils {
 
 /// COMMON ARITHMETIC TYPES ///
 using real8 = float;
-using sint8  = int8_t;
+using sint8 = int8_t;
 using uint8 = uint8_t;
 
 //TODO: https://en.cppreference.com/w/cpp/types/floating-point
 using real16 = float;
-using sint16  = int16_t;
+using sint16 = int16_t;
 using uint16 = uint16_t;
 
 using real32 = float;
-using sint32  = int32_t;
+using sint32 = int32_t;
 using uint32 = uint32_t;
 
 using real64 = double;
-using sint64  = int64_t;
+using sint64 = int64_t;
 using uint64 = uint64_t;
 /// ----------------------- ///
 
 
 /// TYPES FOR LOOPS ///
-using idx_8  = sint8;
+using idx_8 = sint8;
 using idx_16 = sint16;
 using idx_32 = sint32;
 using idx_64 = sint64;
